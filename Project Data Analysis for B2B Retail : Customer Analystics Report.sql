a58d28f26a41920aa5850b5d8b374728c65a0209
Ginalfa Aliza Labikha Sanaj <125278556+ginalfaaliza@users.noreply.github.com>
2024-07-31 14:50:50 +0700

    Update Project Data Analysis for B2B Retail : Customer Analystics Report.sql

--- Project Data Analysis for B2B Retail : Customer Analystics Report.sql
+++ Project Data Analysis for B2B Retail : Customer Analystics Report.sql
@@ -1,96 +1,74 @@
-# Overview tables
-
+# Overview of Table
 SELECT*FROM orders_1 limit 5;
 SELECT*FROM orders_2 limit 5;
 SELECT*FROM customer limit 5;
 
-# Total Penjualan dan Revenue pada Quarter 1 (Jan, Feb, Mar) dan Quarter 2 (Apr, Mei, Jun)
-
---- Quater 1
+# Total sales and Revenue in the Quarter 1 and Quarter 2
+--- Q1
 SELECT
-	SUM(quantity) AS total_penjualan,
-	SUM(quantity*priceeach) AS revenue
+	SUM(quantity) AS total_penjualan, SUM(quantity*priceeach) AS revenue
 FROM orders_1
-WHERE status="Shipped";
-
---- Quater 2
+WHERE status= "Shipped";
+--- Q2
 SELECT
-	SUM(quantity) AS total_penjualan,
-	SUM(quantity*priceeach) AS revenue
+	SUM(quantity) AS total_penjualan, SUM(quantity*priceeach) AS revenue
 FROM orders_2
-WHERE status="Shipped";
+WHERE status= "Shipped";
 
-# Menghitung persentasi keseluruhan penjualan 
-
+# Precentage of Total Sales
 SELECT 
 	quarter,
 	SUM(quantity) AS total_penjualan,
 	SUM(quantity*priceeach) AS revenue
-FROM (SELECT orderNumber,status,quantity,priceeach,'1' AS quarter 
+FROM(
+  	SELECT orderNumber,status,quantity,priceeach,'1' AS quarter
 	FROM orders_1
 	WHERE status='Shipped'
-        UNION
+UNION
 	SELECT orderNumber,status,quantity,priceeach,'2' AS quarter
 	FROM orders_2
 	WHERE status='Shipped') AS tabel_a
 GROUP BY quarter;
 
-# Apakah jumlah customer xyz.com semakin bertambah?
-
-SELECT quarter, COUNT(DISTINCT customerID) AS total_customers
+# Customers Growth
+SELECT 
+	quarter,
+	COUNT(DISTINCT customerID) AS total_customers
 FROM(SELECT
 	 customerID,
 	 createDate,
 	 quarter(createDate) as quarter
 FROM customer
 WHERE createDate BETWEEN '2004-01-01' AND '2004-06-30'
-) AS tabel_b
+)AS tabel_b
 GROUP BY quarter;
 
-# Seberapa banyak customer yang sudah melakukan transaksi?
-
-SELECT quarter,COUNT(DISTINCT customerID) AS total_customers
-FROM(SELECT customerID, createDate, quarter(createDate) AS quarter 
+# Customer Transactions Record
+SELECT
+	quarter,
+	COUNT(DISTINCT customerID) AS total_customers
+FROM(
+  	SELECT customerID,createDate,quarter(createDate) AS quarter
 	FROM customer
-	WHERE createDate BETWEEN '2004-01-01'AND'2004-06-30') AS tabel_b
-WHERE customerID IN(
-	SELECT DISTINCT customerID FROM orders_1
-	UNION
-	SELECT DISTINCT customerID FROM orders_2)
+	WHERE createDate BETWEEN '2004-01-01'AND'2004-06-30')AS tabel_b
+WHERE customerID IN(SELECT DISTINCT customerID FROM orders_1
+UNION
+SELECT DISTINCT customerID FROM orders_2)
 GROUP BY quarter;
 
-#Category produk apa saja yang paling banyak di order oleh customers di quarter 2
-
+# The highest Category Product Sales
 SELECT * 
-FROM (
-SELECT categoryID, COUNT(DISTINCT orderNumber) AS total_order, SUM(quantity) AS total_penjualan 
-      FROM ( 
-       SELECT productCode, orderNumber, quantity,status, 
-       LEFT(productCode,3) AS categoryID
-	FROM orders_2
-	WHERE status = "Shipped") tabel_c
-	GROUP BY categoryID ) tabel_c
+FROM(SELECT 
+  	categoryID, 
+  	COUNT(DISTINCT orderNumber) AS total_order, 
+  	SUM(quantity) AS total_penjualan 
+     FROM (SELECT 
+		productCode,
+		orderNumber,
+		quantity,status, 
+		LEFT(productCode,3) AS categoryID
+	   FROM orders_2
+           WHERE status = "Shipped")c
+     GROUP BY categoryID)b
 ORDER BY total_order DESC;
-
-# Seberapa banyak customers yang tetap aktif bertransaksi setelah transaksi pertama
-SELECT COUNT(DISTINCT customerID) as total_customers FROM orders_1;
-
-SELECT "1" as quarter, (COUNT(DISTINCT customerID)*100)/25 as Q2
-FROM orders_1
-WHERE customerID IN(SELECT DISTINCT customerID FROM orders_2);
-
-
-					
-
-
-
-
-
-
-
-
-
-
-
-
 
